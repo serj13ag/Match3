4df480a4f88cdbb160b65e44107d5a4e38804a05
serj13 <serj13serj@yandex.ru>
2023-02-18 22:05:00 +0400

    5_board - add images, add board and tile, generate tiles on start

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField] private int _width;
+    [SerializeField] private int _height;
+
+    [SerializeField] private Tile _tilePrefab;
+
+    private Tile[,] _tiles;
+
+    private void Start()
+    {
+        SetupTiles();
+    }
+
+    private void SetupTiles()
+    {
+        _tiles = new Tile[_width, _height];
+
+        for (var i = 0; i < _width; i++)
+        {
+            for (var j = 0; j < _height; j++)
+            {
+                Tile tile = Instantiate(_tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
+                tile.name = $"Tile ({i}, {j})";
+                tile.transform.SetParent(transform);
+
+                _tiles[i, j] = tile;
+            }
+        }
+    }
+}

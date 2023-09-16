4bf54a3ba7535534079f8b3b9ac850a15b1f52af
Sergey Agafonov <serj13serj@yandex.ru>
2023-09-16 13:17:20 +0400

    add coin rotate to loading curtain

--- Assets/Scripts/Services/Mono/LoadingCurtainMonoService.cs
+++ Assets/Scripts/Services/Mono/LoadingCurtainMonoService.cs
@@ -8,6 +8,7 @@ namespace Services.Mono
     public class LoadingCurtainMonoService : MonoBehaviour, ILoadingCurtainMonoService
     {
         [SerializeField] private MaskableGraphic _fadeMaskableGraphic;
+        [SerializeField] private GameObject _rotatingCoinsContainer;
 
         private void Awake()
         {
@@ -17,6 +18,7 @@ namespace Services.Mono
         public void FadeOnInstantly()
         {
             _fadeMaskableGraphic.CrossFadeAlpha(Settings.ScreenFader.SolidAlpha, 0f, true);
+            _rotatingCoinsContainer.gameObject.SetActive(true);
         }
 
         public void FadeOnWithDelay()
@@ -34,6 +36,7 @@ namespace Services.Mono
             yield return new WaitForSeconds(Settings.ScreenFader.Delay);
 
             _fadeMaskableGraphic.CrossFadeAlpha(alpha, Settings.ScreenFader.TimeToFade, true);
+            _rotatingCoinsContainer.gameObject.SetActive(false);
         }
     }
 }

a7b67ac341235bc06ba67b792bcf8f3a8a5c7385
Sergey Agafonov <serj13serj@yandex.ru>
2023-09-19 21:02:45 +0400

    add buy background items logic to shop window

--- Assets/Scripts/UI/BackgroundShopItem.cs
+++ Assets/Scripts/UI/BackgroundShopItem.cs
@@ -1,4 +1,7 @@
-﻿using StaticData.Shop;
+﻿using Services;
+using StaticData.Shop;
+using TMPro;
+using UI.Windows;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +11,66 @@ namespace UI
     {
         [SerializeField] private Image _backgroundImage;
 
-        public void Init(BackgroundShopItemStaticData backgroundShopItem)
+        [SerializeField] private Button _button;
+        [SerializeField] private TMP_Text _buttonText;
+
+        private BackgroundShopItemStaticData _backgroundShopItem;
+        private ShopWindow _shopWindow;
+        private bool _isPurchased;
+        private IPurchaseService _purchaseService;
+        private ICoinService _coinService;
+
+        private void OnEnable()
         {
+            _button.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        public void Init(ShopWindow shopWindow, BackgroundShopItemStaticData backgroundShopItem,
+            ICoinService coinService, IPurchaseService purchaseService)
+        {
+            _coinService = coinService;
+            _purchaseService = purchaseService;
+            _shopWindow = shopWindow;
+            _backgroundShopItem = backgroundShopItem;
+
             _backgroundImage.color = backgroundShopItem.Color;
+
+            UpdateView();
+        }
+
+        public void UpdateView()
+        {
+            if (_purchaseService.ItemIsPurchased(_backgroundShopItem.Code))
+            {
+                _isPurchased = true;
+                _buttonText.text = string.Empty;
+                _button.interactable = true;
+            }
+            else
+            {
+                _isPurchased = false;
+                _buttonText.text = _backgroundShopItem.CoinPrice.ToString();
+
+                bool canBuyItem = _coinService.Coins >= _backgroundShopItem.CoinPrice;
+                _button.interactable = canBuyItem;
+            }
+        }
+
+        private void OnButtonClicked()
+        {
+            if (_isPurchased)
+            {
+                _shopWindow.SelectPurchasedBackgroundItem(_backgroundShopItem.Code);
+            }
+            else
+            {
+                _shopWindow.PurchaseBackgroundItem(_backgroundShopItem.Code);
+            }
         }
     }
 }
--- Assets/Scripts/UI/Windows/ShopWindow.cs
+++ Assets/Scripts/UI/Windows/ShopWindow.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Infrastructure;
 using Services;
 using StaticData.Shop;
@@ -18,6 +19,9 @@ namespace UI.Windows
 
         private IStaticDataService _staticDataService;
         private ICoinService _coinService;
+        private IPurchaseService _purchaseService;
+
+        private List<BackgroundShopItem> _prefabs;
 
         private void OnEnable()
         {
@@ -33,13 +37,37 @@ namespace UI.Windows
         {
             _staticDataService = ServiceLocator.Instance.Get<IStaticDataService>();
             _coinService = ServiceLocator.Instance.Get<ICoinService>();
+            _purchaseService = ServiceLocator.Instance.Get<IPurchaseService>();
+
+            _prefabs = new List<BackgroundShopItem>();
 
             UpdateCoinsCount();
 
+            CreateBackgroundShopItems();
+        }
+
+        public void PurchaseBackgroundItem(string itemCode)
+        {
+            _purchaseService.PurchaseItemAndSave(itemCode);
+
+            foreach (BackgroundShopItem backgroundShopItem in _prefabs)
+            {
+                backgroundShopItem.UpdateView();
+            }
+        }
+
+        public void SelectPurchasedBackgroundItem(string itemCode)
+        {
+            // TODO add
+        }
+
+        private void CreateBackgroundShopItems()
+        {
             foreach (BackgroundShopItemStaticData backgroundShopItem in _staticDataService.BackgroundShopItems.OrderBy(x => x.CoinPrice))
             {
                 BackgroundShopItem prefab = Instantiate(_backgroundShopItemPrefab, _backgroundShopItemsContainer);
-                prefab.Init(backgroundShopItem);
+                prefab.Init(this, backgroundShopItem, _coinService, _purchaseService);
+                _prefabs.Add(prefab);
             }
         }
 
